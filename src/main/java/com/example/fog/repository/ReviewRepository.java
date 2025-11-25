2f111eda63a9fc10ac39fa1ad1ec46abc6132afa
Suhyeon7 <zxguszx@naver.com>
2025-11-25 17:35:29 +0900

    🐛 Fix: 향수 조회 코드 수정

--- src/main/java/com/example/fog/repository/ReviewRepository.java
+++ src/main/java/com/example/fog/repository/ReviewRepository.java
@@ -4,6 +4,8 @@ import com.example.fog.entity.Review;
 import com.example.fog.entity.Perfume;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByPerfume(Perfume perfume, Sort sort);
+
+    @Query("SELECT r FROM Review r JOIN FETCH r.user WHERE r.perfume = :perfume ORDER BY r.createdAt DESC")
+    List<Review> findByPerfumeWithUser(@Param("perfume") Perfume perfume);
 }
